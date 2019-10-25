94265d1464e22805c0880744eab8a532fccbbba2
Chad Root <chadmroot@gmail.com>
2019-10-25 17:11:38 +0800

    added temp conversion

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ window.addEventListener("load", () => {
   let tempDegree = document.querySelector(".temp-degree");
   let locationTimezone = document.querySelector(".location-timezone");
   let locationTime = document.querySelector(".current-time");
-  let weatherIcon = document.querySelector(".weather-icon");
+  let tempSection = document.querySelector(".temp");
+  const tempSpan = document.querySelector(".temp span");
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
@@ -20,18 +21,37 @@ window.addEventListener("load", () => {
           return response.json();
       })
         .then(data => {
-          const {temperature, time, summary, icon} = data.currently;
-          console.log(data);
-          // Set DOM elements from API
-          tempDegree.textContent = Math.round(temperature);
-          tempDescription.textContent = summary;
-          weatherIcon.textContent = icon;
-          
-          locationTime.textContent = time; // need to convert from timestamp
+        const {temperature, time, summary, icon} = data.currently;
+        console.log(data);
+        // Set DOM elements from API
+        tempDegree.textContent = Math.round(temperature);
+        tempDescription.textContent = summary;
+        locationTime.textContent = time; // need to convert from timestamp
+        locationTimezone.textContent = data.timezone;
 
-          const {timezone} = data;
-          locationTimezone.textContent = timezone;
+        // Set Icon
+        setIcons(icon, document.querySelector(".icon"));
+
+        // Set Temp Conversion
+        let celsius = (temperature - 32) * (5/9);
+        // Change Temp C - F
+        tempSection.addEventListener('click', () => {
+          if(tempSpan.textContent === "F"){
+            tempSpan.textContent = "C";
+            tempDegree.textContent = Math.round(celsius);
+          } else {
+            tempSpan.textContent = "F";
+            tempDegree.textContent = Math.round(temperature);
+          }
+        });
       });
     });
+  }
+
+  function setIcons(icon, iconID) {
+    const skycons = new Skycons ({color: "white"});
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+    skycons.play();
+    return skycons.set(iconID, Skycons[currentIcon]);
   }
 });
