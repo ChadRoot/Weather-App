c1315da42fff2d1f8bae5c6f8507d27791c99ff8
Chad Root <chadmroot@gmail.com>
2021-08-02 18:14:28 +0800

    search functionality

--- app.js
+++ app.js
@@ -16,16 +16,31 @@ let weather = {
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    console.log(name, icon, description, temp, humidity, speed);
     document.querySelector(".city").innerText = "Weather in " + name;
     document.querySelector(".weather-icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector(".temp").innerText = temp + "°C";
     document.querySelector(".description").innerText = description;
     document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
     document.querySelector(".wind").innerText = "Wind Speed: " + speed + "km/hr";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
   }
 };
 
+document.querySelector(".search-button").addEventListener("click", function () {
+  weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+  if (event.key == "Enter") {
+    weather.search();
+  }
+});
+
+weather.fetchWeather("Los Angeles");
 
 
 
