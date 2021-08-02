d859383ae12ab17fba9a2d093fdd2817f34eee62
Chad Root <chadmroot@gmail.com>
2021-08-02 18:17:41 +0800

    rounded numbers

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ let weather = {
     const { speed } = data.wind;
     document.querySelector(".city").innerText = "Weather in " + name;
     document.querySelector(".weather-icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".temp").innerText = (Math.round(temp)) + "°C";
     document.querySelector(".description").innerText = description;
     document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-    document.querySelector(".wind").innerText = "Wind Speed: " + speed + "km/hr";
+    document.querySelector(".wind").innerText = "Wind Speed: " + (Math.round(speed)) + "km/hr";
     document.querySelector(".weather").classList.remove("loading");
     document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
   },
